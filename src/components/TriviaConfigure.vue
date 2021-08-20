09cf49c5845707b3908a7e3cf53e008ec7dfde1a
T0wn <eirijomine@hotmail.com>
2021-08-20 14:39:50 +0200

    router setup

---
+++ src/components/TriviaConfigure.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <form></form>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "TriviaConfigure",
+    data() {
+        return {
+            categories: ["placeholder", "placeholder2"],
+        };
+    },
+};
+</script>
